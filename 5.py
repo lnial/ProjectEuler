728dc840370ec30c7fcb5eea2736d237dd238229
Junya Yamamoto <nitro.mic.ug+github@gmail.com>
2011-05-26 00:43:38 +0900

    make debug class

--- 5.py
+++ 5.py
@@ -1,18 +1,36 @@
+import time
+
 quotient = 10
 flagEnd = 0
+degug = TimePrintDebug()
 
 while 1:
   quotient += 1
   count = 0
-  for i in range(1, 11):
+
+  degug.tb = datetime.datetime.now()
+  degug.value = quotient
+
+
+  for i in range(1, 21):
     if quotient % i == 0:
       count += 1
-    if count == 10:
+    if count == 20:
       flagEnd = 1
   if flagEnd == 1:
     break
 
 print quotient
+
+
+class TimePrintDebug()
+  def __init__(self, time, ta, tb, value)
+  #if init time, ta, tb
+  ta = datetime.datetime.now()
+  self.time = tb - ta
+  if self.time >= 1:
+    print self.value
+
 
 #for i in range(1, 10):
   #print i
