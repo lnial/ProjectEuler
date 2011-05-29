29fdf1ebf5feca1e314a41892fabb5fbbfc9f1d6
Junya Yamamoto <nitro.mic.ug+github@gmail.com>
2011-05-29 19:53:44 +0900

    comp

--- 6.py
+++ 6.py
@@ -1,2 +1,19 @@
-i += sumM
-j = sumAll * sumAll
+def factorial(x):
+  if x == 0:
+    return 0
+  else:
+    return x + factorial(x - 1)
+ 
+def factorialRaise(x):
+  y = x**2
+  if y == 0:
+    return 0
+  else:
+    return y + factorialRaise(x - 1)
+
+factorialSum = factorial(100)
+factorialSum = factorialSum **2
+#print factorialSum
+
+#print factorialRaise(100)
+print factorialSum - factorialRaise(100)
