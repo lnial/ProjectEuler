53771630a20f87f3fc0cf10e75a2c721e56d77c7
Junya Yamamoto <nitro.mic.ug+github@gmail.com>
2011-06-03 21:40:00 +0900

    NW test

--- 3.py
+++ 3.py
@@ -1,28 +1,33 @@
 debug = 1
 primFactor = 13195
+#primFactor = 600851475143L
 list1 = []
 list1.append(1)
 
 for i in range(2, primFactor):
 
-    print i
     if primFactor % i == 0:
-        print i
-        for j in list1:
-            print list1
-            #print j
-            if list1[0] == 1:
-                print 'ok'
-                list1.append(i)
-            elif j % i == 0:
-                #print 'OK'
-                break
-            else:
-                #print i
-                #print "@@@@"
-                list1.append(i)
-    elif debug == 1:
-        break
+      list1.append(i)
        
+
+#dispose
+list1.remove(1)
 print list1
+#list2 = list1[:]
+#list1.reverse()
+#print range(list1)
+
+#for j in range(list1[0], list1):
+  #print j
+  #for k in range(list2):
+    #if j % k == 0:
+      #print "@@@"
+      #list1.remove(j)
+    
+
+  
+
+#print j
+  #if  % j != 0:
+    #list1.append(i)
 
--- 7.py
+++ 7.py
@@ -1,2 +1,30 @@
 
+def primeNum(x):
+  # generate Prime List
+  list1 = [1, 2]
+  
+  return list1[x - 1]
 
+#print primeNum(1)
+
+i = 1
+flag = 0
+listPrime = [2, 3, 5]
+while 1:
+  i += 1
+
+  checkFlag = 0
+  for j in listPrime:
+    if i % j != 0 and checkFlag == 0:
+      checkFlag = 1
+
+      if len(listPrime) == 6:
+        flag = 1
+        break
+  if checkFlag == 1:
+    listPrime.append(i)
+
+  if flag == 1:
+    break
+
+print listPrime
