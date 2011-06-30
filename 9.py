b76d57dedb59a0a183677b3ed1b9cabfece34fde
Junya Yamamoto <nitro.mic.ug+github@gmail.com>
2011-06-30 17:34:37 +0900

    comp

---
+++ 9.py
@@ -0,0 +1,14 @@
+import sys
+
+result = []
+
+for a in range(1, 1000):
+  for b in range(1, 1000):
+    c = 1000 - a -b
+
+    if a < b and b < c:
+      if c**2 == a**2 + b**2:
+        print a*b*c
+        sys.exit()
+
+
