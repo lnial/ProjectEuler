f0861f04b688fb10d10c511f0f2a01821fadb1c7
lnial <lnialainl@gmail.com>
2011-11-23 14:18:53 +0900

    Not comp

--- 18.py
+++ 18.py
@@ -4,22 +4,33 @@
 
 next_index = 0
 sumn = 0
+line_dir = []
 
 for line in open('input18.txt', 'r'):
     line_arr = line[:-1].split(" ")
-
-    if len(line_arr) != 1:
-        print line_arr[next_index], line_arr[next_index + 1]
-        next_index = line_arr.index(max(line_arr[next_index], line_arr[next_index + 1]))
-        sumn += int(line_arr[next_index])
-        "next_i" + str(next_index)
-
-    else:
-        sumn += int(line_arr[0])
-        
-
-print sumn
+    line_dir.append(line_arr)
     
 
+
+
+
+    #if len(line_arr) != 1:
+        #print line_arr[next_index], line_arr[next_index + 1]
+        #next_index = line_arr.index(max(line_arr[next_index], line_arr[next_index + 1]))
+        #sumn += int(line_arr[next_index])
+        #"next_i" + str(next_index)
+
+    #else:
+        #sumn += int(line_arr[0])
+
+for i in xrange(len(line_dir), 0 , -1):
+    print line_dir[i-1], len(line_dir[i-1])
+    for j in xrange(0, len(line_dir[i-1])):
+        print line_dir[i-1][j]
+
+    #for j in line_dir[i-1]:
+        ##print line_dir[i-1][
+
+        #print j
 
 
