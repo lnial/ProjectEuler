a7cdbb7547e9e586bb6fa476bf6eeabccb4c41a0
lnial <lnialainl@gmail.com>
2011-11-26 21:40:24 +0900

    diff test

--- 18.py
+++ 18.py
@@ -11,8 +11,6 @@ for line in open('input18.txt', 'r'):
     line_dir.append(line_arr)
     
 
-pass
-
     #if len(line_arr) != 1:
         #print line_arr[next_index], line_arr[next_index + 1]
         #next_index = line_arr.index(max(line_arr[next_index], line_arr[next_index + 1]))
@@ -31,5 +29,14 @@ for i in xrange(len(line_dir), 0 , -1):
         ##print line_dir[i-1][
 
         #print j
+
+
+class tedhst():
+    """class documentation"""
+    def __init__(self, a):
+        """__init__ documentation"""
+        pass
+
+
 
 
